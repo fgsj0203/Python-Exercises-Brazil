f3d93a08fc0ae81f1d01a817cbb8f6c0f9d7fdae
Francisco Gomes <fgsj.dev@outlook.com>
2022-02-17 19:01:30 -0300

    Print numbers odd between 1 and 50

---
+++ repetitionStructure/file06.py
@@ -0,0 +1,11 @@
+"""
+Author = Francisco Gomes
+Date = 17/02/2022
+description = print of 50 firsts numbers
+version = 1.0
+"""
+
+# Structure logical for print numbers odd
+for x in range(0,50):
+    if (x % 2 == 1):
+        print(x)
