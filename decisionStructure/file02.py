cb51f51a7a5c405fd8a065e9d33d2cbfd5ce35c0
Francisco Gomes <fgsj.dev@outlook.com>
2022-02-16 23:43:52 -0300

    Displaying number is positive or negative

---
+++ decisionStructure/file02.py
@@ -0,0 +1,19 @@
+"""
+Author = Francisco Gomes
+Date = 16/02/2022
+description = input number and printing value is positive or negative
+version = 1.0
+"""
+
+# Variables
+numberValue = int(input("Number: "))
+
+# Logical decision
+if numberValue >= 0:
+    print("Number is: ", numberValue)
+    print("The number is positive")
+else:
+    print("Number is: ",numberValue)
+    print("The number is negative")
+
+
